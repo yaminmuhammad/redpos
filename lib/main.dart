c2518b52caf0649f830db98b07425469606bd7ad
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-05-08 15:00:57 +0700

    add style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:print_bluetooth_thermal/post_code.dart';
+import 'package:redpos/core/constants/colors.dart';
 import 'package:redpos/presentation/auth/login_page.dart';
 
 void main() {
@@ -14,23 +17,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
         useMaterial3: true,
+        textTheme: GoogleFonts.quicksandTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        appBarTheme: AppBarTheme(
+          color: AppColors.white,
+          elevation: 0,
+          titleTextStyle: GoogleFonts.quicksand(
+            color: AppColors.primary,
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+          iconTheme: const IconThemeData(
+            color: AppColors.primary,
+          ),
+        ),
       ),
       home: const LoginPage(),
     );
