9bbf56b6aad335a085e7b1148a43342758d019ba
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-05-09 15:12:29 +0700

    add variables && bloc state 2942

---
+++ lib/core/constants/variable.dart
@@ -0,0 +1,5 @@
+class Variables {
+  static const String appName = 'redpos';
+  static const String apiVersion = 'v1';
+  static const String baseUrl = 'https://sbi-pos.ptsiu.id';
+}
--- lib/data/datasource/auth_remote_datasource.dart
+++ lib/data/datasource/auth_remote_datasource.dart
@@ -1,11 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:redpos/data/models/response/auth_response_model.dart';
+import 'package:redpos/core/constants/variable.dart';
 
 class AuthRemoteDataSource {
   Future<Either<String, AuthResponseModel>> login(
       String username, String password) async {
-    final url = Uri.parse('https://sbi-pos.ptsiu.id/auth/sign-in');
+    final url = Uri.parse('${Variables.baseUrl}/auth/sign-in');
     final response = await http.post(
       url,
       body: {
@@ -16,7 +17,7 @@ class AuthRemoteDataSource {
     if (response.statusCode == 200) {
       return Right(AuthResponseModel.fromJson(response.body));
     } else {
-      return Left('Failed to Login');
+      return const Left('Failed to Login');
     }
   }
 }
