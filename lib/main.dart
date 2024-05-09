1de30fb560f6b99da75c7f8ef62bf40a2cc45787
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-05-09 09:38:07 +0700

    add model response & http

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:print_bluetooth_thermal/post_code.dart';
 import 'package:redpos/core/constants/colors.dart';
 import 'package:redpos/presentation/auth/login_page.dart';
 
@@ -15,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
--- lib/presentation/auth/login_page.dart
+++ lib/presentation/auth/login_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 // import 'package:flutter_posresto_app/presentation/home/dashboard_page.dart';
 import 'package:redpos/presentation/home/dashboard_page.dart';
@@ -33,32 +35,36 @@ class _LoginPageState extends State<LoginPage> {
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 124.0, vertical: 20.0),
         children: [
-          const SpaceHeight(80.0),
           Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 130.0),
-              child: SvgPicture.asset(
-                Assets.icons.homeResto.path,
-                width: 100,
-                height: 100,
-                color: AppColors.red,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 130.0,
+                vertical: 90.0,
+              ),
+              child: Image.network(
+                'https://sbi-pos.ptsiu.id/storage/2024/04/whatsapp-image-2024-03-16-at-162840-1.png',
+                width: 90.0,
+                height: 90.0,
               )),
-          const SpaceHeight(24.0),
-          const SpaceHeight(8.0),
-          const Center(
-            child: Text(
-              'Aplikasi Kasir',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
-            ),
-          ),
+          // const SpaceHeight(80.0),
+          // const SpaceHeight(24.0),
+          // const SpaceHeight(8.0),
+          // const Center(
+          //   child: Text(
+          //     'Aplikasi Kasir',
+          //     style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          //   ),
+          // ),
           const SpaceHeight(40.0),
           CustomTextField(
             controller: usernameController,
             label: 'Username',
+            hintText: 'Masukkan Username',
           ),
           const SpaceHeight(12.0),
           CustomTextField(
             controller: passwordController,
             label: 'Password',
+            hintText: 'Masukkan Password',
             obscureText: true,
           ),
           const SpaceHeight(24.0),
